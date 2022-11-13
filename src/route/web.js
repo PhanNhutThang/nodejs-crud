71a62d25d3f55c56265578bc3ed049c90b6c3f0f
PhanNhutThang <phannhutthang12a4@gmail.com>
2022-11-13 21:23:05 +0700

    nodejs2

--- src/route/web.js
+++ src/route/web.js
@@ -1,5 +1,6 @@
 import express from "express";
 import homeController from "../controllers/homeController";
+import userController from "../controllers/userController";
 
 let router = express.Router();
 
@@ -12,6 +13,13 @@ let initWebRoutes = (app) => {
     router.get('/edit-crud', homeController.getEditCrud);
     router.post('/put-crud', homeController.putcrud);
     router.get('/delete-crud', homeController.deletecrud);
+
+    router.post('/api/login', userController.handleLogin)
+    router.get('/api/get-all-users', userController.handleGetAllUsers);
+    router.post('/api/create-new-user', userController.handleCreateNewUser);
+    router.put('/api/edit-user', userController.handleEditUser);
+    router.delete('/api/delete-user', userController.handleDeleteUser);
+
     return app.use("/", router);
 }
 module.exports = initWebRoutes;
