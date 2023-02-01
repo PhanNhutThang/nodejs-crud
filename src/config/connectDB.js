380d8671eefe970cb665b90967f1f92384084789
PhanNhutThang <phannhutthang12a4@gmail.com>
2023-02-01 16:23:21 +0700

    not-vercel

--- src/config/connectDB.js
+++ src/config/connectDB.js
@@ -5,17 +5,17 @@ const sequelize = new Sequelize('nhutthangtestnodejs', 'root', null, {
     dialect: 'mysql',
     logging: false
 });
-const mysql = require('mysql2')
+// const mysql = require('mysql2')
 
-const pool = mysql.createPool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DBNAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0
-});
+// const pool = mysql.createPool({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DB_DBNAME,
+//     waitForConnections: true,
+//     connectionLimit: 10,
+//     queueLimit: 0
+// });
 let connectDB = async () => {
     try {
         await sequelize.authenticate();
@@ -24,12 +24,12 @@ let connectDB = async () => {
         console.error('Unable to connect to the database:', error);
     }
 }
-pool.getConnection((err, conn) => {
-    if (err) console.log(err)
-    console.log("Connected successfully")
-})
+// pool.getConnection((err, conn) => {
+//     if (err) console.log(err)
+//     console.log("Connected successfully")
+// })
 
-module.exports = connectDB, pool.promise()
+module.exports = connectDB
 
 // const mysql = require('mysql2')
 
